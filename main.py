9ad10a6f41e7f04755210824bd6a0c37a17e1fbd
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2022-11-28 10:19:12 +0100

    zadanie 1

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 #test
+#pandas + numpy + matplotlib/seaborn
+#netmico + paramico
+#
+# print('whatever')
+# print('cos jeszcze')
+#przerwa do 10:11
+# print("hello!")
+# print('hello!')
+# print("Mc'Donalds")
+# x='Andrzej'
+# y=10
+# print(x)
+# print(y)
+# print('Witaj '+x+"!")
+# print('Witaj {}!'.format(x))
+# print(f'Witaj {x}!')
+# print(type(y))
+# print('y='+str(y))
+# print('y={}'.format(y))
+# print(f'y={y}')
+# owoc=input('podaj swój ulubiony owoc:\n')
+# print(f'Twój ulubiony owoc to {owoc}')
+# print('Twój ulubiony owoc to {}'.format(owoc))
+# print('Twój ulubiony owoc to '+owoc)
+
+#1. Napisz program który przyjmie od użyszkodnika imię oraz nazwisko, a następnie
+#   wypisze na konsoli komunikat typu "Witaj TwojeImie TwojeNazwisko!"
