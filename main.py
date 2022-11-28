1ef3bc01f6f19015c0da41d443994dce040aa0c9
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2022-11-28 10:55:58 +0100

    zadanie 3

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-#test
-#pandas + numpy + matplotlib/seaborn
-#netmico + paramico
+# test
+# pandas + numpy + matplotlib/seaborn
+# netmico + paramico
 #
 # print('whatever')
 # print('cos jeszcze')
-#przerwa do 10:11
+# przerwa do 10:11
 # print("hello!")
 # print('hello!')
 # print("Mc'Donalds")
@@ -24,7 +24,7 @@
 # print('Twój ulubiony owoc to {}'.format(owoc))
 # print('Twój ulubiony owoc to '+owoc)
 
-#1. Napisz program który przyjmie od użyszkodnika imię oraz nazwisko, a następnie
+# 1. Napisz program który przyjmie od użyszkodnika imię oraz nazwisko, a następnie
 #   wypisze na konsoli komunikat typu "Witaj TwojeImie TwojeNazwisko!"
 #
 # imie=input('podaj imię:\n')
@@ -33,7 +33,7 @@
 # print('Witaj {} {}!'.format(imie,nazwisko))
 # print('Witaj '+imie+" "+nazwisko+"!")
 
-#print(imie+" "+nazwisko)
+# print(imie+" "+nazwisko)
 
 # x=input('dej x:\n')
 # y=input('dej y:\n')
@@ -49,7 +49,50 @@
 # print(wynik)
 # print(pow(2,10))
 
-#2. BMI= masa/(wzrost*wzrost) . Napisz program który odbierze od użytkownika jego masę w kilogramach
+# 2. BMI= masa/(wzrost*wzrost) . Napisz program który odbierze od użytkownika jego masę w kilogramach
 # i wzrost w metrach, wyliczy i wypisze BMI.
 
-#wzrost=1.76
+# wzrost=1.76
+
+# wzrost = float(input('podaj wzrost w metrach:\n'))
+# masa = float(input('podaj masę w kilogramach:\n'))
+# bmi=round(masa/pow(wzrost,2),2)
+# print(f'bmi={bmi}')
+#
+
+#
+# x=1
+# if x == 1:
+#     print('jeden')
+#     print('jeden')
+#     print('jeden')
+# print('koniec')
+
+
+# x=2
+# if x == 1:
+#     print('jeden')
+# else:
+#     print('nie jeden')
+# print('koniec')
+
+# x=2
+# if x == 1:
+#     print('jeden')
+# elif x==2:
+#     print('dwa')
+# elif x==3:
+#     print('trzy')
+# else:
+#     print('poza zakresem')
+# print('koniec')
+#
+# x=1
+# y=1
+# if x==1:
+#     print('x=1')
+# if y==1:
+#     print('y=1')
+
+#3. Niech użytkownik poda jakąś liczbę. Jeśli poda dodatnią to chcemy wyświetlić tę wartość z informacją "wartość dodatnia",
+# jeśli zero to wyświetlamy z informacją "równe zero", jeśli ujemna to wyświetlamy "wartość ujemna".
